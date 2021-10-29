56e06c06b6cc513fe6d756e8803a2191376f4eb5
Marky Mark <gamermaker.dev@gmail.com>
2021-10-29 14:48:53 -0400

    deploy

---
+++ precache-manifest.e6cb7933a22d99586a9275c5cce88b4c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a07c83bcd8682f9b45f",
+    "url": "/css/index.22a34bac.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "9fa0cb6ebf9093dcea45248ccb42435f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8485d2100d11bbd7dfd3",
+    "url": "/js/chunk-vendors.8a294f8b.js"
+  },
+  {
+    "revision": "1a07c83bcd8682f9b45f",
+    "url": "/js/index.632f5f3c.js"
+  },
+  {
+    "revision": "1b55e35a1633ab2a05bcef0a7451b069",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "053100cb84a50d2ae7f5492f7dd7f25e",
+    "url": "/site.webmanifest"
+  }
+]);
